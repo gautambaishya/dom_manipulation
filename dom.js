298699685da0d79a1dc26133cf91a7c469de0542
GautamBaishya <gautambaishya893@gmail.com>
2022-05-21 16:20:34 +0530

    GetElementByTagName

--- dom.js
+++ dom.js
@@ -37,3 +37,26 @@
 
 
 
+// GetElementsByClassName 
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// // items[1].textContent = 'Hello 2';
+// // items[1].style.fontWeight = 'bold';
+// items[2].style.background = 'green';
+
+// for(var i = 0; i<items.length; i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+// GetElementByTageName
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+li[2].style.background = 'red';
+
+for(var i = 0; i<li.length; i++){
+    li[i].style.fontWeight = 'bold';
+}
